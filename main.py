2fda4f4530648801c90d14c69fd814a1322c138c
Jeff Tieng <jtieng1377@conestogac.on.ca>
2025-07-22 07:53:30 -0400

    Styling updates
    1) Added weather icons and flag icons
    2) Updated the color scheme of the website
    3) Updated the html code to format content properly
    4) Web extension coming soon

--- main.py
+++ main.py
@@ -23,13 +23,25 @@ def getWeather():
     if not weather_data["cod"] == 200:
         return render_template("city_not_found.html")
 
+    icon_url = getWeatherIcon(weather_data["weather"][0]["icon"])
+
     return render_template(
         "weather.html",
         title=weather_data["name"],
+        country=weather_data["sys"]["country"],
         status=weather_data["weather"][0]["description"].title(),
         temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['main']['feels_like']}",
+        temp_low=f"{weather_data['main']['temp_min']:.1f}",
+        temp_high=f"{weather_data['main']['temp_max']:.1f}",
+        feels_like=f"{weather_data['main']['feels_like']:.1f}",
+        icon_url=icon_url,
     )
+
+
+def getWeatherIcon(icon_code):
+
+    icon_url = f"https://openweathermap.org/img/wn/{icon_code}@2x.png"
+    return icon_url
 
 
 if __name__ == "__main__":
